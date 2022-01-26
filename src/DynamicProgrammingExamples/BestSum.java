cec20d33ed17b3e12179e7bf0d253ba9220f96b7
Baktyiar Tentimishov <baktyiar.tentimishov@flatexdegiro.com>
2022-01-27 00:50:50 +0100

    dynamic programming sample BestSum solution

---
+++ src/DynamicProgrammingExamples/BestSum.java
@@ -0,0 +1,91 @@
+package DynamicProgrammingExamples;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+/*
+m = target sum
+n = numbers.length
+
+Brute Force
+time: O(n^m * m)
+space: O(m^2)
+
+Memoized
+time: O(m^2 * n)
+space: O(m^2)
+
+
+*/
+public class BestSum {
+
+    public static void main(String[] args) {
+        int[] arr2 = {5, 3,4,7};
+        int[] arr3 = {2, 3, 5};
+        int[] arr4 = {1, 4, 5};
+        int[] arr5 = {1,2,5,25};
+        BestSum bestSum = new BestSum();
+
+        System.out.println(bestSum.sum(7, arr2, new HashMap<>())); // [7]
+        System.out.println(bestSum.sum(8, arr3, new HashMap<>())); // [3, 5]
+        System.out.println(bestSum.sum(8, arr4, new HashMap<>())); // [4, 4]
+        System.out.println(bestSum.sum(100, arr5, new HashMap<>())); //[25, 25, 25, 25]
+    }
+
+    private ArrayList<Integer> bestSum(int targetSum, int[] numbers, HashMap<Integer, ArrayList<Integer>> memo) {
+
+        if (memo.containsKey(targetSum)) return memo.get(targetSum);
+        if (targetSum == 0) return new ArrayList<Integer>();
+        if (targetSum < 0) return null;
+
+        ArrayList<Integer> shortestCombination = null;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int remainder = targetSum - numbers[i];
+            ArrayList<Integer> remainderCombination = bestSum(remainder, numbers, memo);
+            if (remainderCombination != null) {
+
+
+                remainderCombination.add(numbers[i]);
+
+                if (shortestCombination == null || remainderCombination.size() < shortestCombination.size()) {
+                    shortestCombination = new ArrayList<Integer>();
+                    shortestCombination = (ArrayList<Integer>) remainderCombination.clone();
+                }
+            }
+        }
+
+
+        memo.put(targetSum, shortestCombination);
+        return shortestCombination;
+    }
+
+    public ArrayList<Integer> sum (int targetSum, int[] numbers, HashMap<Integer, ArrayList<Integer>> memo) {
+        if(memo.containsKey(targetSum)) return memo.get(targetSum);
+        if(targetSum == 0) return new ArrayList<>();
+        if(targetSum < 0) return null;
+
+
+        ArrayList<Integer> shortestCombination = null;
+
+        for(int i  = 0; i < numbers.length; i++) {
+            int remainder = targetSum-numbers[i];
+            ArrayList<Integer> remainderCombination = sum(remainder, numbers, memo);
+            if(remainderCombination != null) {
+
+                ArrayList<Integer> combination;
+                combination = (ArrayList<Integer>)remainderCombination.clone();
+
+
+                combination.add(numbers[i]);
+
+                if(shortestCombination == null || combination.size() < shortestCombination.size()){
+                    shortestCombination = (ArrayList<Integer>) combination.clone();
+                }
+            }
+        }
+
+        memo.put(targetSum, shortestCombination);
+        return shortestCombination;
+    }
+
+}
--- src/DynamicProgrammingExamples/HowSum.java
+++ src/DynamicProgrammingExamples/HowSum.java
@@ -1,4 +1,59 @@
 package DynamicProgrammingExamples;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+/*
+m = target sum
+n = numbers.length
+
+Brute Force
+time: O(n^m * m)
+space: O(m)
+
+Memoized
+time: O(n*m^2)
+space: O(m^2)
+
+
+*/
 public class HowSum {
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {2, 3};
+        int[] arr2 = {5, 3,4,7};
+        int[] arr3 = {2, 4};
+        int[] arr4 = {2, 3, 5};
+        int[] arr5 = {7,14};
+        HowSum howSum = new HowSum();
+        System.out.println(howSum.howSum(7, arr1, new HashMap<>()));
+        System.out.println(howSum.howSum(7, arr2, new HashMap<>()));
+        System.out.println(howSum.howSum(7, arr3, new HashMap<>()));
+        System.out.println(howSum.howSum(8, arr4, new HashMap<>()));
+        System.out.println(howSum.howSum(300, arr5, new HashMap<>()));
+    }
+
+
+    private ArrayList<Integer> howSum(int targetSum, int[] numbers, HashMap<Integer, ArrayList<Integer>> memo){
+
+        if (memo == null) memo = new HashMap<>();
+
+        if (memo.containsKey(targetSum)) return memo.get(targetSum);
+        if (targetSum == 0) return new ArrayList<>();
+        if (targetSum < 0) return null;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int remainder = targetSum - numbers[i];
+            ArrayList<Integer> remainderResult = howSum(remainder, numbers, memo);
+
+            if (remainderResult != null) {
+                remainderResult.add(numbers[i]);
+                memo.put(targetSum, remainderResult);
+                return memo.get(targetSum);
+            }
+        }
+        memo.put(targetSum, null);
+        return null;
+    }
+
 }
