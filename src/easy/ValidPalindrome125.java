bda6e45d04049f7f799993fefbe5262912121405
Baktyiar Tentimishov <baktyiar.tentimishov@flatexdegiro.com>
2022-07-21 22:19:41 +0200

    dynamic programming: AllConstruct 1 easy and 1 Medium

--- src/easy/ValidPalindrome125.java
+++ src/easy/ValidPalindrome125.java
@@ -1,12 +1,34 @@
 package easy;
 
+import java.util.Stack;
+
 public class ValidPalindrome125 {
     public static void main(String[] args) {
 
     }
-//
-//    public boolean isPalindrome(String s) {
-//
-//    }
+
+    public boolean isPalindrome(String s) {
+        StringBuilder builder = new StringBuilder();
+        s = s.toLowerCase();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetter(c) || Character.isDigit(c)) {
+                builder.append(c);
+            }
+        }
+
+        String s1 = builder.toString();
+
+        builder = new StringBuilder();
+
+        char[] charArray = s1.toCharArray();
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            char c = charArray[i];
+            builder.append(c);
+        }
+        String s2 = builder.toString();
+
+
+        return s1.equals(s2);
+    }
 
 }
