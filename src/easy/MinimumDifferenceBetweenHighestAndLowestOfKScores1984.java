88127af891c525f77878c7cab4ca86399f7057d5
Baktyiar Tentimishov <baktyiar.tentimishov@flatexdegiro.com>
2022-08-01 23:26:38 +0200

    1 easy

--- src/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.java
+++ src/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class MinimumDifferenceBetweenHighestAndLowestOfKScores1984 {
 
     public static void main(String[] args) {
+        MinimumDifferenceBetweenHighestAndLowestOfKScores1984 minimum = new MinimumDifferenceBetweenHighestAndLowestOfKScores1984();
+        System.out.println(minimum.minimumDifference(new int[]{9, 4, 1, 7}, 2));
 
     }
 
@@ -13,24 +15,17 @@ public class MinimumDifferenceBetweenHighestAndLowestOfKScores1984 {
         Arrays.sort(nums);
 
 
-        int max = nums[0];
         int min = nums[0];
-
-        for (int i : nums){
-            if (max < i) {
-                max = i;
-            }
-            if (min > i) {
-                min = i;
-            }
-        }
+        int max = nums[k - 1];
 
         int difference = max - min;
 
-        for (int i = 1; i <= nums.length + k; i++) {
-            max = nums[i];
-        }
+        for (int i = 1; i <= nums.length - k; i++) {
+            min = nums[i];
+            max = nums[(i + k) - 1];
 
+            difference = Math.min(difference, max - min);
+        }
         return difference;
     }
 }
